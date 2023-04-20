33ec1775bfe8046045bcadce18ffbc079c5af0aa
Milton-Mills <miltonmills9924@gmail.com>
2023-04-19 19:52:44 -0700

    changed the homepage details and css

--- Desktop/ADAM/web/js/scripts.js
+++ Desktop/ADAM/web/js/scripts.js
@@ -15,3 +15,20 @@ function toggleMenu() {
 
 // Event listeners
 toggle.addEventListener("click", toggleMenu, false);
+
+const backToTopButton = document.createElement('button');
+backToTopButton.innerHTML = 'Back to top';
+backToTopButton.classList.add('back-to-top');
+document.body.appendChild(backToTopButton);
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
