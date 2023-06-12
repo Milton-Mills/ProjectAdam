25a3c08510e4dbe2acf6729fed5b97768db5abde
Milton-Mills <miltonmills9924@gmail.com>
2023-06-12 06:33:39 -0700

    changes to login panel, authentication working.

--- Desktop/ADAM/web/js/scripts.js
+++ Desktop/ADAM/web/js/scripts.js
@@ -32,3 +32,18 @@ window.addEventListener('scroll', () => {
 backToTopButton.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+const form = document.getElementById("login-form");
+const errorMessage = document.getElementById("error-message");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const username = form.username.value;
+  const password = form.password.value;
+
+  if (username === "Milton" && password === "12345") {
+    window.location.href = "index.html";
+  } else {
+    errorMessage.innerText = "Invalid username or password";
+  }
+});
